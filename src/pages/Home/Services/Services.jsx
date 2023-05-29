47109689e5b3d5854574b1448011107c4e960419
md-abir-alahe <mdabiralahe@gmail.com>
2023-05-29 23:59:33 +0600

    services data is loading from local host

--- src/pages/Home/Services/Services.jsx
+++ src/pages/Home/Services/Services.jsx
@@ -7,7 +7,7 @@ const Services = () => {
     const [services, setServices] = useState([]);
 
     useEffect(()=>{
-        fetch('https://car-doctor-server-delta-coral.vercel.app/services')
+        fetch('http://localhost:5000/services')
         .then(res => res.json())
         .then(data => setServices(data))
     },[]);
